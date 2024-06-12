e5f5f55fdd88a9c94a0e7c80d9d81dc77c55f01b
JioChoi <jiochoi7@gmail.com>
2024-06-12 21:47:35 +0900

    express init

---
+++ index.js
@@ -0,0 +1,16 @@
+const PORT = process.env.PORT || 80;
+const express = require('express');
+
+const app = express();
+
+app.use('/css', express.static(__dirname + '/src/css'));
+app.use('/js', express.static(__dirname + '/src/js'));
+app.use('/assets', express.static(__dirname + '/src/assets'));
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/src/index.html');
+});
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
