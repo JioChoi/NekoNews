8150ddb37d3f4f522444bc030bcb52d72ca6c60e
JioChoi <jiochoi7@gmail.com>
2024-06-18 16:22:41 +0900

    Change image finding prompt

--- src/js/list.js
+++ src/js/list.js
@@ -1,15 +1,38 @@
+const loadSize = 20;
+
+let listPos = 0;
+let listEnd = false;
+
 document.addEventListener('DOMContentLoaded', function () {
+	loadArticles(listPos, loadSize);
+
 	for(let i = 0; i < 2; i++)
 		addCardItem('https://plus.unsplash.com/premium_photo-1669865741911-fdc00c7c3859?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxMjA3fDB8MXxzZWFyY2h8MXx8RWFydGhxdWFrZSUyMHxlbnwwfHx8fDE3MTgxNjMyMDJ8MA&ixlib=rb-4.0.3&q=80&w=1080', "충격적인 고양이 뉴스다냥!!", "qrqrqrqr");
-
-	for(let i = 0; i < 10; i++)
-		addListItem('https://plus.unsplash.com/premium_photo-1669865741911-fdc00c7c3859?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxMjA3fDB8MXxzZWFyY2h8MXx8RWFydGhxdWFrZSUyMHxlbnwwfHx8fDE3MTgxNjMyMDJ8MA&ixlib=rb-4.0.3&q=80&w=1080', "충격적인 고양이 뉴스다냥!!", "2024.04.05 24:24:53", "qrqrqrqr");
 
 	const more = document.getElementById('more');
 	more.addEventListener('click', function () {
 		more.innerText = '로딩중...';
 	});
 });
+
+async function loadArticles(start, size) {
+	let response = await fetch(host + '/api/articles?' + new URLSearchParams({ start: start, size: size }), {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
+	response = await response.json();
+
+	listPos += response.length;
+	if (response.length < size) {
+		listEnd = true;
+	}
+
+	for (let i = 0; i < response.length; i++) {
+		addListItem(response[i].image, response[i].title, response[i].time, response[i].id);
+	}
+}
 
 function addListItem(img, title, date, id) {
 	const article = document.createElement('div');
